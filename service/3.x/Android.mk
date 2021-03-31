4e1562731cda1657bdb94b95dc699281297f4f8d
Bob Badour <bbadour@google.com>
2021-03-31 12:41:20 -0700

    gralloc: merge specific CLs on AndroidS to gralloc4.0 [2/8]
    PD#SWPL-67690
    
    Problem:
    merge specific CLs on AndroidS to gralloc4.0
    
    Solution:
    [LSC] Add LOCAL_LICENSE_KINDS to hardware/amlogic
    
    Verify:
    AndroidS ohm & boreal
    
    Change-Id: Idda7565eff8ed76a28fd783db62438b172fdbb47
    Signed-off-by: binqi zhang <binqi.zhang@amlogic.com>

--- service/3.x/Android.mk
+++ service/3.x/Android.mk
@@ -3,6 +3,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := android.hardware.graphics.allocator@3.0-service
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0
+LOCAL_LICENSE_CONDITIONS := notice
 LOCAL_INIT_RC := android.hardware.graphics.allocator@3.0-service.rc
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_RELATIVE_PATH := hw
--- src/capabilities/Android.mk
+++ src/capabilities/Android.mk
@@ -19,6 +19,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libgralloc_capabilities
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0
+LOCAL_LICENSE_CONDITIONS := notice
 LOCAL_MODULE_OWNER := arm
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE_TAGS := optional
