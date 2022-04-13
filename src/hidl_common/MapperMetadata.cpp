17b725e335dd8cdbc0f83d22827d9cb5f8e099f8
binqi zhang <binqi.zhang@amlogic.com>
2022-04-13 15:00:27 +0800

    gralloc: fix getAndroidYCbCr_P010 failed in VTS [1/1]
    PD#SWPL-75716
    PD#SWPL-78167
    
    Problem:
    no CR compoents for DRM_FORMAT_P010
    
    Solution:
    tmp workaround patch to change one of the CB to CR
    
    Verify:
    AndroidT ohm
    
    Change-Id: I3ebbe63f808bc5284a9ec2580f9220324663de82
    Signed-off-by: binqi zhang <binqi.zhang@amlogic.com>

--- src/hidl_common/MapperMetadata.cpp
+++ src/hidl_common/MapperMetadata.cpp
@@ -168,7 +168,7 @@ static std::vector<std::vector<PlaneLayoutComponent>> plane_layout_components_fr
 			.drm_fourcc = DRM_FORMAT_P010,
 			.components = {
 				{ { Y, 6, 10 } },
-				{ { CB, 6, 10 }, { CB, 22, 10 } }
+				{ { CB, 6, 10 }, { CR, 22, 10 } }
 			}
 		},
 		/* Planar 8 bit YUV 4:2:0 */
