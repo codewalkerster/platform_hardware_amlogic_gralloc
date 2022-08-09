7796d3e39b6911bdb907688049dfe24feed3267f
Liang Ji <liang.ji@amlogic.com>
2022-08-09 09:47:19 +0800

    gralloc: sync google code back to aml [1/1]
    PD#SWPL-90209
    
    Problem:
    build error caused by gralloc code
    'aidl/arm/graphics/Compression.h' file not found
    
    Solution:
    use "arm.graphics-V1-ndk"
    
    Verify:
    ohm-U
    
    Signed-off-by: Liang Ji <liang.ji@amlogic.com>
    Change-Id: I85af76f5505ebcc1ecd2c4409a76923fd210d947

--- gralloc.go
+++ gralloc.go
@@ -25,19 +25,17 @@ func hw_gralloc_aml_Defaults(ctx android.LoadHookContext) {
     PlatformVndkVersion := ctx.DeviceConfig().PlatformVndkVersion()
     fmt.Println("PlatformVndkVersion:", PlatformVndkVersion)
     IntPlatformVndkVersion,err := strconv.Atoi(PlatformVndkVersion)
+    // For An Android letter, before freeze API PlatformVndkVersion return code name, like
+    // "Tiramisu", after freeze API it has been changed to number.
     if err != nil {
         fmt.Printf("%v fail to convert", IntPlatformVndkVersion)
-        return
+        p.Shared_libs = append(p.Shared_libs, "arm.graphics-V1-ndk")
+    } else {
+        if IntPlatformVndkVersion > 32 {
+            p.Shared_libs = append(p.Shared_libs, "arm.graphics-V1-ndk")
+        } else {
+            p.Shared_libs = append(p.Shared_libs, "arm.graphics-V1-ndk_platform")
+        }
     }
-    //For Andriod T, before freeze API PlatformVndkVersion return string like "Tiramisu",
-    //after freeze API it has been changed to number "33"
-    // if len(PlatformVndkVersion) > 2 {
-    fmt.Printf("%v success to convert", IntPlatformVndkVersion)
-    if IntPlatformVndkVersion > 32 {
-         p.Shared_libs = append(p.Shared_libs, "arm.graphics-V1-ndk")
-     } else {
-         p.Shared_libs = append(p.Shared_libs, "arm.graphics-V1-ndk_platform")
-     }
-
     ctx.AppendProperties(p)
 }
