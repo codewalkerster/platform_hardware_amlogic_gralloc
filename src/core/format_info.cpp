e956edce9013cf444778ec594e5611d89e8c93a0
yang.zhou <yang.zhou@amlogic.com>
2023-02-02 11:41:07 +0000

    gralloc: fix camera won't start [1/1]
    PD#SWPL-100538
    
    Problem:
    camera won't start
    
    Solution:
    add cam_wr=F_LIN
    
    Verify:
    AndroidU adt4
    
    Change-Id: Iab0788dc83a79686f234214eee48b1c2133d97aa
    Signed-off-by: yang.zhou <yang.zhou@amlogic.com>

--- src/core/format_info.cpp
+++ src/core/format_info.cpp
@@ -496,7 +496,7 @@ const format_ip_support_t formats_ip_support[] = {
 		.dpu_aeu_wr = F_NONE,
 		.vpu_rd = F_LIN,
 		.vpu_wr = F_LIN,
-		.cam_wr = F_NONE,
+		.cam_wr = F_LIN,
 	},
 	{
 		.id = MALI_GRALLOC_FORMAT_INTERNAL_YU12,
