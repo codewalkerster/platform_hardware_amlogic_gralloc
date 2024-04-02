8af32923aac6c5a48cb243bc13dc1c3fdda0f3e1
Kiyoung Kim <kiyoungkim@google.com>
2024-04-02 10:43:43 +0900

    Do not use Platform VNDK version
    VNDK is deprecated from 24Q2, and Platform VNDK version is not a version
    for SDK version. This change updates version related build code to get
    proper version (Platform SDK version) instead of deprecated one. Some
    code is removed if it is for old version, as current branch will not
    build againt old platform version.
    
    Bug: 330100430
    Test: m nothing passed
    Change-Id: I99540ef2711a22c92d392a9355a9e3dee94deb1f

--- gralloc.go
+++ gralloc.go
@@ -1,41 +1,27 @@
 package hw_gralloc
 
 import (
-    "android/soong/android"
-    "android/soong/cc"
-    //"fmt"
-    "strconv"
+	"android/soong/android"
+	"android/soong/cc"
+	//"fmt"
 )
 
 func init() {
-    android.RegisterModuleType("hw_gralloc_go_defaults",gralloc_DefaultsFactory)
+	android.RegisterModuleType("hw_gralloc_go_defaults", gralloc_DefaultsFactory)
 }
 
-func gralloc_DefaultsFactory() (android.Module) {
-    module := cc.DefaultsFactory()
-    android.AddLoadHook(module, hw_gralloc_aml_Defaults)
-    return module
+func gralloc_DefaultsFactory() android.Module {
+	module := cc.DefaultsFactory()
+	android.AddLoadHook(module, hw_gralloc_aml_Defaults)
+	return module
 }
 
 func hw_gralloc_aml_Defaults(ctx android.LoadHookContext) {
-    type propsE struct {
-        Shared_libs  []string
-    }
-    p := &propsE{}
-    PlatformVndkVersion := ctx.DeviceConfig().PlatformVndkVersion()
-    //fmt.Println("PlatformVndkVersion:", PlatformVndkVersion)
-    IntPlatformVndkVersion,err := strconv.Atoi(PlatformVndkVersion)
-    // For An Android letter, before freeze API PlatformVndkVersion return code name, like
-    // "Tiramisu", after freeze API it has been changed to number.
-    if err != nil {
-        //fmt.Printf("%v fail to convert", IntPlatformVndkVersion)
-        p.Shared_libs = append(p.Shared_libs, "arm.graphics-V1-ndk")
-    } else {
-        if IntPlatformVndkVersion > 32 {
-            p.Shared_libs = append(p.Shared_libs, "arm.graphics-V4-ndk")
-        } else {
-            p.Shared_libs = append(p.Shared_libs, "arm.graphics-V1-ndk_platform")
-        }
-    }
-    ctx.AppendProperties(p)
+	type propsE struct {
+		Shared_libs []string
+	}
+	p := &propsE{}
+
+	p.Shared_libs = append(p.Shared_libs, "arm.graphics-V4-ndk")
+	ctx.AppendProperties(p)
 }
